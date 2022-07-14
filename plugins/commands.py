118f9c5c6153ab2433a84cde4a862a3fe12ba894
BLVCK1100 <108452308+BLVCK1100@users.noreply.github.com>
2022-07-14 13:18:29 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -46,8 +46,8 @@ async def start(client, message):
         ]]
               
         reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_video(
-            video="https://telegra.ph/file/6a627a84a0ecaab240cb1.mp4",
+        await message.reply_photo(
+            photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention),
             reply_markup=reply_markup,
             parse_mode='html'
